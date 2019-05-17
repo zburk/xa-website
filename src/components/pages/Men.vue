94695ec6fe71e518cbaa51cc220ce3654dab5b30
Zachary Burk <zachburk7@gmail.com>
2019-05-17 16:45:31 -0400

    Add men and women

--- src/components/pages/Men.vue
+++ src/components/pages/Men.vue
@@ -9,12 +9,55 @@
         ></base-banner>
       </div>
     </div>
+
+    <div class="flex">
+      <h2 class="flex-1 text-center py-24">Mondays at 6:30 in Connor</h2>
+    </div>
+
+    <div class="container mx-auto mb-24">
+      <div class="flex flex-row items-center mb-10" v-for="(member, index) in leaders" :key="index">
+        <div class="flex-none px-6">
+          <base-member
+            :member="member">
+          </base-member>
+        </div>
+        <div class="flex-grow">
+          {{ member.text }}
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-grey-lighter">
+      <div class="container mx-auto pb-24">
+        <div class="flex flex-col">
+          <h2 class="flex-1 text-center py-24">What We're Studying</h2>
+
+          <p class="flex-1 leading-normal">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Men'
+  name: 'Men',
+  data() {
+    return {
+      leaders: [
+        {
+          name: 'Christopher Piccirillo',
+          headshot: 'https://via.placeholder.com/200x200',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.'
+        },
+        {
+          name: 'Stephen Kenney',
+          headshot: 'https://via.placeholder.com/200x200',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.'
+        }
+      ]
+    }
+  }
 }
 </script>
 
--- src/components/pages/Women.vue
+++ src/components/pages/Women.vue
@@ -9,12 +9,70 @@
         ></base-banner>
       </div>
     </div>
+
+    <div class="flex">
+      <h2 class="flex-1 text-center py-24">Mondays at 6:30 in Rams Village 5</h2>
+    </div>
+
+    <div class="container mx-auto mb-24">
+      <div class="flex flex-row items-center mb-10" v-for="(member, index) in leaders" :key="index">
+        <div class="flex-none px-6">
+          <base-member
+            :member="member">
+          </base-member>
+        </div>
+        <div class="flex-grow">
+          {{ member.text }}
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-grey-lighter">
+      <div class="container mx-auto pb-24">
+        <div class="flex flex-col">
+          <h2 class="flex-1 text-center py-24">What We're Studying</h2>
+
+          <p class="flex-1 leading-normal">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Women'
+  name: 'Men',
+  data() {
+    return {
+      leaders: [
+        {
+          name: 'Jane Stonehouse',
+          headshot: 'https://via.placeholder.com/200x200',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.'
+        },
+        {
+          name: 'Megan Wood',
+          headshot: 'https://via.placeholder.com/200x200',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.'
+        },
+        {
+          name: 'Tynsley Gilchrist',
+          headshot: 'https://via.placeholder.com/200x200',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.'
+        },
+        {
+          name: 'Madelyn Smith',
+          headshot: 'https://via.placeholder.com/200x200',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.'
+        },
+        {
+          name: 'Mackenzie Harris',
+          headshot: 'https://via.placeholder.com/200x200',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.'
+        }
+      ]
+    }
+  }
 }
 </script>
 
