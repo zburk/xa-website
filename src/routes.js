ce9b6fea43516e54b447f7c1afc4ffd73d965b3a
Zachary Burk <zachburk7@gmail.com>
2019-05-17 17:18:10 -0400

    Add meta data

--- src/routes.js
+++ src/routes.js
@@ -14,7 +14,17 @@ export default {
             component: Home,
             name: 'home',
             meta: {
-                title: 'Home | Chi Alpha at UNC'
+                title: 'Home | Chi Alpha at UNC',
+                metaTags: [
+                    {
+                      name: 'description',
+                      content: 'Our vision for Chi Alpha is to learn together to be Devoted Disciples of Jesus who build Deep Friendships with those around us and act as Deliberate Servants of our campus, community, and world. Chi Alpha is a ministry that exists to reach college students, build them up in their faith and send them out to serve and make a difference in a world in great need.'
+                    },
+                    {
+                      property: 'og:description',
+                      content: 'Our vision for Chi Alpha is to learn together to be Devoted Disciples of Jesus who build Deep Friendships with those around us and act as Deliberate Servants of our campus, community, and world. Chi Alpha is a ministry that exists to reach college students, build them up in their faith and send them out to serve and make a difference in a world in great need.'
+                    }
+                ]
             }
         },
         {
@@ -22,7 +32,17 @@ export default {
             component: About,
             name: 'about',
             meta: {
-                title: 'About | Chi Alpha at UNC'
+                title: 'About | Chi Alpha at UNC',
+                metaTags: [
+                    {
+                      name: 'description',
+                      content: 'Our vision for Chi Alpha is to learn together to be Devoted Disciples of Jesus who build Deep Friendships with those around us and act as Deliberate Servants of our campus, community, and world. Chi Alpha is a ministry that exists to reach college students, build them up in their faith and send them out to serve and make a difference in a world in great need.'
+                    },
+                    {
+                      property: 'og:description',
+                      content: 'Our vision for Chi Alpha is to learn together to be Devoted Disciples of Jesus who build Deep Friendships with those around us and act as Deliberate Servants of our campus, community, and world. Chi Alpha is a ministry that exists to reach college students, build them up in their faith and send them out to serve and make a difference in a world in great need.'
+                    }
+                ]
             }
         },
         {
@@ -30,7 +50,17 @@ export default {
             component: Staff,
             name: 'staff',
             meta: {
-                title: 'Staff | Chi Alpha at UNC'
+                title: 'Staff | Chi Alpha at UNC',
+                metaTags: [
+                    {
+                      name: 'description',
+                      content: 'Our vision for Chi Alpha is to learn together to be Devoted Disciples of Jesus who build Deep Friendships with those around us and act as Deliberate Servants of our campus, community, and world. Chi Alpha is a ministry that exists to reach college students, build them up in their faith and send them out to serve and make a difference in a world in great need.'
+                    },
+                    {
+                      property: 'og:description',
+                      content: 'Our vision for Chi Alpha is to learn together to be Devoted Disciples of Jesus who build Deep Friendships with those around us and act as Deliberate Servants of our campus, community, and world. Chi Alpha is a ministry that exists to reach college students, build them up in their faith and send them out to serve and make a difference in a world in great need.'
+                    }
+                ]
             }
         },
         {
@@ -38,7 +68,17 @@ export default {
             component: Men,
             name: 'men',
             meta: {
-                title: 'Men | Chi Alpha at UNC'
+                title: 'Men | Chi Alpha at UNC',
+                metaTags: [
+                    {
+                      name: 'description',
+                      content: 'Our vision for Chi Alpha is to learn together to be Devoted Disciples of Jesus who build Deep Friendships with those around us and act as Deliberate Servants of our campus, community, and world. Chi Alpha is a ministry that exists to reach college students, build them up in their faith and send them out to serve and make a difference in a world in great need.'
+                    },
+                    {
+                      property: 'og:description',
+                      content: 'Our vision for Chi Alpha is to learn together to be Devoted Disciples of Jesus who build Deep Friendships with those around us and act as Deliberate Servants of our campus, community, and world. Chi Alpha is a ministry that exists to reach college students, build them up in their faith and send them out to serve and make a difference in a world in great need.'
+                    }
+                ]
             }
         },
         {
@@ -46,7 +86,17 @@ export default {
             component: Women,
             name: 'women',
             meta: {
-                title: 'Women | Chi Alpha at UNC'
+                title: 'Women | Chi Alpha at UNC',
+                metaTags: [
+                    {
+                      name: 'description',
+                      content: 'Our vision for Chi Alpha is to learn together to be Devoted Disciples of Jesus who build Deep Friendships with those around us and act as Deliberate Servants of our campus, community, and world. Chi Alpha is a ministry that exists to reach college students, build them up in their faith and send them out to serve and make a difference in a world in great need.'
+                    },
+                    {
+                      property: 'og:description',
+                      content: 'Our vision for Chi Alpha is to learn together to be Devoted Disciples of Jesus who build Deep Friendships with those around us and act as Deliberate Servants of our campus, community, and world. Chi Alpha is a ministry that exists to reach college students, build them up in their faith and send them out to serve and make a difference in a world in great need.'
+                    }
+                ]
             }
         },
         {
@@ -54,7 +104,17 @@ export default {
             component: Blank,
             name: 'give',
             meta: {
-                title: 'Give | Chi Alpha at UNC'
+                title: 'Give | Chi Alpha at UNC',
+                metaTags: [
+                    {
+                      name: 'description',
+                      content: 'Our vision for Chi Alpha is to learn together to be Devoted Disciples of Jesus who build Deep Friendships with those around us and act as Deliberate Servants of our campus, community, and world. Chi Alpha is a ministry that exists to reach college students, build them up in their faith and send them out to serve and make a difference in a world in great need.'
+                    },
+                    {
+                      property: 'og:description',
+                      content: 'Our vision for Chi Alpha is to learn together to be Devoted Disciples of Jesus who build Deep Friendships with those around us and act as Deliberate Servants of our campus, community, and world. Chi Alpha is a ministry that exists to reach college students, build them up in their faith and send them out to serve and make a difference in a world in great need.'
+                    }
+                ]
             }
         }
     ]
