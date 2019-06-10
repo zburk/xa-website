116a269e35f90652f915b5a8368ff93fe4b5dda4
Zachary Burk <zachburk7@gmail.com>
2019-06-10 19:31:19 -0400

    Add links to resources

--- src/components/pages/Resources.vue
+++ src/components/pages/Resources.vue
@@ -11,23 +11,41 @@
       <div class="flex-1 py-24">
         <div class="container mx-auto leading-normal">
           <h3>Topical</h3>
-          <p><strong>Campus Gods:</strong> This is a seven-week study. We were all made to worship. The question is, “What are we worshipping?” If we are not worshipping God, we are worshipping something else. The Bible calls this idolatry. This study is designed to look at some of the idols that students on college campuses face. Topics include: worshipping the god of achievement; worshipping the god of sex; worshipping the god of substances; worshipping the god of information; and more.</p>
+          <p><strong>Campus Gods:</strong>&nbsp;
+            <a href="http://campusministry.org/docs/tools/Campus-gods.pdf" target="_blank" class="text-blue">(Link)</a> 
+            This is a seven-week study. We were all made to worship. The question is, “What are we worshipping?” If we are not worshipping God, we are worshipping something else. The Bible calls this idolatry. This study is designed to look at some of the idols that students on college campuses face. Topics include: worshipping the god of achievement; worshipping the god of sex; worshipping the god of substances; worshipping the god of information; and more.</p>
 
-          <p><strong>Foundations in Christ:</strong> This is a seven-week study. Once we’ve said yes to Christ, what do we do next? This study explores what it means to build a life with Christ as the foundation. Topics include: balancing God’s grace and our works; getting acclimated to our new life in Jesus; understanding ourselves as children of God; the uniqueness of Jesus; what it means to follow him; turning our old ways over to accept new ways; and our role in passing on the message of Jesus.</p>
+          <p><strong>Foundations in Christ:</strong>&nbsp;
+            <a href="https://docs.google.com/document/d/1gYNNi9W4U6FGCevGwOK8P9nqu_yzhAfnPeuctASjrbQ/edit#" target="_blank" class="text-blue">(Link)</a>
+            This is a seven-week study. Once we’ve said yes to Christ, what do we do next? This study explores what it means to build a life with Christ as the foundation. Topics include: balancing God’s grace and our works; getting acclimated to our new life in Jesus; understanding ourselves as children of God; the uniqueness of Jesus; what it means to follow him; turning our old ways over to accept new ways; and our role in passing on the message of Jesus.</p>
 
           <h3>Intro</h3>
-          <p><strong>Alpha:</strong> Alpha is an eight to ten week video based study covering the basics of the Christian faith. This study is wonderful for people who are new to faith, exploring Christian faith for the first time, or who want to revisit the more foundational questions of faith. Topics include: Who is Jesus?, Why did Jesus die?, How and why should I read the Bible?, Does God heal today?, and more.</p>
+          <p><strong>Alpha:</strong>&nbsp;
+          <a href="http://www.alpha.org/" target="_blank" class="text-blue">(Link)</a> 
+          Alpha is an eight to ten week video based study covering the basics of the Christian faith. This study is wonderful for people who are new to faith, exploring Christian faith for the first time, or who want to revisit the more foundational questions of faith. Topics include: Who is Jesus?, Why did Jesus die?, How and why should I read the Bible?, Does God heal today?, and more.</p>
 
-          <p><strong>Redemptive Story in 10 images:</strong> This is a ten-week study. Starting in Genesis and ending in Revelation, this study covers the grand story of the Bible using powerful images and themes to stick in Life Group members’ minds. Images include: image, thorns, lamb, covenant, temple, anointing, cross, communion, map, tree.</p>
+          <p class="text-grey-darker">*You will need to create their own account to have access to all the videos</p>
+
+          <p><strong>Redemptive Story in 10 images:</strong>&nbsp;
+          <a href="https://irp-cdn.multiscreensite.com/2988a589/files/uploaded/images.pdf" target="_blank" class="text-blue">(Link)</a> 
+          This is a ten-week study. Starting in Genesis and ending in Revelation, this study covers the grand story of the Bible using powerful images and themes to stick in Life Group members’ minds. Images include: image, thorns, lamb, covenant, temple, anointing, cross, communion, map, tree.</p>
 
           <h3>Books of the Bible</h3>
-          <p><strong>Daniel:</strong> This is a seven-week study. This study covers the first-half of the book of Daniel (chapters 1-6, 9). Daniel’s story tells us what it’s like to be devoted to God, but live in culture that does not know nor honor God. Topics include: living counter-culturally; seeking wisdom; identifying idols; speaking truth into someone; learning to be holy; trusting God in difficult situations; learning to pray for the campus.</p>
+          <p><strong>Daniel:</strong>&nbsp;
+          <a href="https://irp-cdn.multiscreensite.com/2988a589/files/uploaded/Daniel%20Study.pdf" target="_blank" class="text-blue">(Link)</a> 
+          This is a six-week study. This study covers the first-half of the book of Daniel (chapters 1-6). Daniel’s story tells us what it’s like to be devoted to God, but live in culture that does not know nor honor God. Topics include: living counter-culturally; seeking wisdom; identifying idols; speaking truth into someone; learning to be holy; trusting God in difficult situations; learning to pray for the campus.</p>
 
-          <p><strong>Exodus:</strong> This is a eight-week study. This study tells the story of God choosing Moses to liberate his people from the tyranny of Pharaoh and the Egyptian lifestyle. The study starts with God appearing to Moses in the form of the burning bush and finishes with Moses receiving the Ten Commandments from God (with emphasis on the last five as it reveals how we are to respond in relationship with others). Topics include: an invitation from God; facing our idols; embracing Sabbath and God’s provision; practicing remembrance; and more.</p>
+          <p><strong>Exodus:</strong>&nbsp;
+          <a href="https://irp-cdn.multiscreensite.com/2988a589/files/uploaded/Exodus.pdf" target="_blank" class="text-blue">(Link)</a> 
+          This is a eight-week study. This study tells the story of God choosing Moses to liberate his people from the tyranny of Pharaoh and the Egyptian lifestyle. The study starts with God appearing to Moses in the form of the burning bush and finishes with Moses receiving the Ten Commandments from God (with emphasis on the last five as it reveals how we are to respond in relationship with others). Topics include: an invitation from God; facing our idols; embracing Sabbath and God’s provision; practicing remembrance; and more.</p>
 
-          <p><strong>James:</strong> This is a twelve-week video study. The resource used is a Youversion Bible plan featuring Francis Chan. Topics include: facing our temptations, controlling our reactions, the sin of favoritism, pairing faith with action, the power of our words, seeking true wisdom, searching for humility, opening ourselves to God’s plans, chasing after riches, suffering and patience, prayer and faith.</p>
+          <p><strong>James:</strong>&nbsp;
+          <a href="https://www.bible.com/reading-plans/14480-book-of-james-francis-chan-12-day-video-study" target="_blank" class="text-blue">(Link)</a> 
+          This is a twelve-week video study. The resource used is a Youversion Bible plan featuring Francis Chan. Topics include: facing our temptations, controlling our reactions, the sin of favoritism, pairing faith with action, the power of our words, seeking true wisdom, searching for humility, opening ourselves to God’s plans, chasing after riches, suffering and patience, prayer and faith.</p>
 
-          <p><strong>Galatians:</strong> This is a nine-week study. This study walks through the entire book of Galatians. Paul’s letter to the Galatians admonishes them to remain true to the gospel, that they are saved by grace through faith, even when they are tempted to try to earn God’s approval through their own good works. Topics include: what the gospel is, transformative power of grace, grace through faith, and identity in Christ.</p>
+          <p><strong>Galatians:</strong>&nbsp;
+          <a href="http://www.grace-bible.org/uploads/publications/galatians_final_8-22-15.pdf" target="_blank" class="text-blue">(Link)</a> 
+          This is a nine-week study. This study walks through the entire book of Galatians. Paul’s letter to the Galatians admonishes them to remain true to the gospel, that they are saved by grace through faith, even when they are tempted to try to earn God’s approval through their own good works. Topics include: what the gospel is, transformative power of grace, grace through faith, and identity in Christ.</p>
         </div>
       </div>
     </div>
