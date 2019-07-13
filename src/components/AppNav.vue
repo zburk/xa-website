6ae5f69fc906d6d76954b452020d49cd4aa28647
Zachary Burk <zachburk7@gmail.com>
2019-07-13 16:48:45 -0400

    Add testimonials to nav

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -3,13 +3,14 @@
     <div class="flex-1 pl-5">
       <a href="/"><span class="text-xl text-white">Chi Alpha at UNC</span></a>
     </div>
-    <div class="flex-1 pl-10 pr-6">
+    <div class="flex-grow pl-10 pr-6">
       <ul class="list-reset text-right flex flex-col lg:flex-row items-center justify-between">
         <li class="py-3 lg:py-0"><router-link :to="{ name: 'home' }" exact class="text-black">Home</router-link></li>
         <li class="py-3 lg:py-0"><router-link :to="{ name: 'about' }" class="text-black">About</router-link></li>
         <li class="py-3 lg:py-0"><router-link :to="{ name: 'staff' }" class="text-black">Staff</router-link></li>
         <li class="py-3 lg:py-0"><router-link :to="{ name: 'men' }" class="text-black">Men</router-link></li>
         <li class="py-3 lg:py-0"><router-link :to="{ name: 'women' }" class="text-black">Women</router-link></li>
+        <li class="py-3 lg:py-0"><router-link :to="{ name: 'testimonials' }" class="text-black">Testimonials</router-link></li>
         <li class="py-3 lg:py-0"><router-link :to="{ name: 'give' }" class="text-black">Give</router-link></li>
         <li class="py-3 lg:py-0"><router-link :to="{ name: 'resources' }" class="text-black">Resources</router-link></li>
       </ul>
